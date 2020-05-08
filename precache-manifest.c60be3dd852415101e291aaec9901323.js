9f30d27e14b46824abd9b8c24921ad71f7d0f083
Aniruddha Mukherjee <aniruddha.xccentric@gmail.com>
2020-05-08 18:15:31 +0530

    Updates

---
+++ precache-manifest.c60be3dd852415101e291aaec9901323.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7be96d2f5cb1dcb8775db18065b17e1",
+    "url": "/game-story/index.html"
+  },
+  {
+    "revision": "17862befa009dd3a80c8",
+    "url": "/game-story/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "991f8c64ee23da935027",
+    "url": "/game-story/static/css/main.02ebc1aa.chunk.css"
+  },
+  {
+    "revision": "17862befa009dd3a80c8",
+    "url": "/game-story/static/js/2.c25b57a6.chunk.js"
+  },
+  {
+    "revision": "98867ad4f1a4e59d80dc68747cfef1ae",
+    "url": "/game-story/static/js/2.c25b57a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "991f8c64ee23da935027",
+    "url": "/game-story/static/js/main.5e1c39db.chunk.js"
+  },
+  {
+    "revision": "8651dc11c3b7fcd1cce8",
+    "url": "/game-story/static/js/runtime-main.c7182350.js"
+  },
+  {
+    "revision": "cdb413ff583a81c3e8f1c88bc58775bc",
+    "url": "/game-story/static/media/brands-and-logotypes.cdb413ff.svg"
+  },
+  {
+    "revision": "c3fc7584e91aa65747265a777f906a0c",
+    "url": "/game-story/static/media/down-arrow.c3fc7584.svg"
+  },
+  {
+    "revision": "e02c1a30bbd5072236004711b22aa83c",
+    "url": "/game-story/static/media/symbol.e02c1a30.svg"
+  },
+  {
+    "revision": "6a3966fb7a834f454712c4596dd67d99",
+    "url": "/game-story/static/media/undraw_profile_pic_ic5t.6a3966fb.svg"
+  }
+]);
